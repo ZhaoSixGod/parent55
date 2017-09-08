08e5050f01fb549698e9437e500b4fdb7fa2a74d
ght <596906605@qq.com>
2017-09-08 17:08:54 +0800

    后台页面搭建
    在Controller中跳转后台系统页面

--- babasport-console/src/main/java/com/itheima/babasport/controller/CenterController.java
+++ babasport-console/src/main/java/com/itheima/babasport/controller/CenterController.java
@@ -17,7 +17,67 @@ import com.itheima.babasport.service.TestTbService;
 @Controller
 public class CenterController {
 	
-	@Autowired
+	//入口页面
+	@RequestMapping(value="/control/index.do")
+	public String index(){
+		return "index";
+	}
+	//入口页面->头
+	@RequestMapping(value="/control/top.do")
+	public String top(){
+		return "top";
+	}
+	//入口页面->身体
+	@RequestMapping(value="/control/main.do")
+	public String main(){
+		return "main";
+	}
+	//入口页面->身体->左
+	@RequestMapping(value="/control/left.do")
+	public String left(){
+		return "left";
+	}
+	//入口页面->身体->右
+	@RequestMapping(value="/control/right.do")
+	public String right(){
+		return "right";
+	}
+	//商品->身体
+	@RequestMapping(value="/control/frame/product_main.do")
+	public String product_main(){
+		return "frame/product_main";
+	}
+	//商品->身体->左
+	@RequestMapping(value="/control/frame/product_left.do")
+	public String product_left(){
+		return "frame/product_left";
+	}
+	//广告->身体
+	@RequestMapping(value="/control/frame/ad_main.do")
+	public String ad_main(){
+		return "frame/ad_main";
+	}
+	//广告->身体->左
+	@RequestMapping(value="/control/frame/ad_left.do")
+	public String ad_left(){
+		return "frame/ad_left";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//注释掉测试方法
+	/*@Autowired
 	private TestTbService testTbService;
 	
 	//入口
@@ -30,5 +90,5 @@ public class CenterController {
 		testTbService.insertTestTb(testTb);
 		
 		return "index";
-	}
+	}*/
 }
