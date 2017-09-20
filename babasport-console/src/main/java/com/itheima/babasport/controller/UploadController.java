4e943fcd997331900564c3e48749b82d8e88b01d
ght <596906605@qq.com>
2017-09-20 09:18:51 +0800

    富文本编辑器上传图片

--- babasport-console/src/main/java/com/itheima/babasport/controller/UploadController.java
+++ babasport-console/src/main/java/com/itheima/babasport/controller/UploadController.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
 import com.itheima.babasport.service.product.UploadService;
 import com.itheima.babasport.web.Constants;
@@ -76,7 +80,29 @@ public class UploadController {
 		return urls;
 	}
 	
-	
+	//上传富文本编辑的多张图片
+	@RequestMapping(value="/upload/uploadFck.do")
+	public void uploadFck(HttpServletRequest request ,HttpServletResponse response ) throws Exception{
+		
+		//无敌版接受图片
+		//spring提供了MultipartRequest
+		MultipartRequest mr=(MultipartRequest)request;
+		//只有图片
+		Map<String, MultipartFile> fileMap = mr.getFileMap();
+		Set<Entry<String,MultipartFile>> entrySet = fileMap.entrySet();
+		for (Entry<String, MultipartFile> entry : entrySet) {
+			MultipartFile pic = entry.getValue();
+			//保存图片到FastDFS中
+			String path = uploadService.uploadPic(pic.getBytes(), pic.getOriginalFilename(), pic.getSize());
+			//回显到富文本编辑器上 json
+			JSONObject jo=new JSONObject();
+			jo.put("url", Constants.IMG_URL+path);
+			jo.put("error", 0);
+			//回调图片
+			response.setContentType("application/json;charset=UTF-8");
+			response.getWriter().write(jo.toString());
+		}
+	}
 	
 	
 	
